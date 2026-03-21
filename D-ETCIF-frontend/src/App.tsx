a620f2e99ca11bcc07918efdd7ae61f498783b3f
0125nia <1205367274@qq.com>
2026-03-22 04:15:39 +0800

    feat(frontend:app): implement main application structure with routing and toast notifications

--- D-ETCIF-frontend/src/App.tsx
+++ D-ETCIF-frontend/src/App.tsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import { useRoutes } from "react-router-dom";
+import routes from "@/router";
+import Toast from "./components/common/Toast";
+import { AnimatePresence } from "framer-motion";
+import React from "react";
 
-function App() {
-  return <></>;
+export default function App() {
+  const element = useRoutes(routes);
+  return (
+    <>
+      <Toast />
+      {/* AnimatePresence 会追踪子元素的卸载，从而触发 exit 动画 */}
+      <AnimatePresence mode="wait">
+        {/* 给 element 绑定 key，让 Framer Motion 知道路由变了 */}
+        {element && React.cloneElement(element, { key: location.pathname })}
+      </AnimatePresence>
+    </>
+  );
 }
-
-export default App;
