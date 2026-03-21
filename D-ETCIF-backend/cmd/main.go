81b0b3c77861de3692fde6376c6590d1e7c0a607
0125nia <1205367274@qq.com>
2026-03-22 04:35:33 +0800

    feat(backend:main): enhance main function with database initialization logs

--- D-ETCIF-backend/cmd/main.go
+++ D-ETCIF-backend/cmd/main.go
@@ -1,6 +1,24 @@
-// Package cmd D-ETCIF-backend/cmd/main.go
+// Package cmd
+// D-ETCIF-backend/cmd/main.go
 package cmd
 
-func main() {
+import (
+	config "D-ETCIF-backend/configs"
+	"D-ETCIF-backend/internal/router"
+	"D-ETCIF-backend/pkg/utils"
 
+	db_config "D-ETCIF-backend/internal/config"
+)
+
+func main() {
+	utils.Info("========开始初始化数据库连接========")
+
+	db_config.InitDB()
+
+	utils.Info("========数据库连接初始化完成========")
+	r := router.NewRouter()
+
+	utils.Info("========启动服务========")
+	defer utils.Info("========服务已停止========")
+	_ = r.Run(config.Config.Server.Port)
 }
