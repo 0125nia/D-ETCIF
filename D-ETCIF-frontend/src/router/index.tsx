91a446e1e7e8dad14ef02222ac1bf39564c4c8f4
0125nia <1205367274@qq.com>
2026-03-23 14:36:50 +0800

    feat(frontend:router): update student and teacher routes with new pages and layouts

--- D-ETCIF-frontend/src/router/index.tsx
+++ D-ETCIF-frontend/src/router/index.tsx
@@ -2,6 +2,18 @@ import type { RouteObject } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import { AuthGuard } from "./AuthGuard";
 
+import StudentLayout from "@/layouts/role/StudentLayout";
+import LabPage from "@/pages/student/lab/LabPage";
+import PreStagePage from "@/pages/student/stage/PreStagePage";
+import DoingStagePage from "@/pages/student/stage/DoingStagePage";
+import PostStagePage from "@/pages/student/stage/PostStagePage";
+import FeedbackPage from "@/pages/student/feedback/FeedbackPage";
+import ProfilePage from "@/pages/student/profile/ProfilePage";
+
+import TeacherLayout from "@/layouts/role/TeacherLayout";
+import Dashboard from "@/pages/teacher/dashborad/Dashborad";
+import HelpPage from "@/pages/teacher/help/HelpPage";
+
 import LoginPage from "@/pages/auth/LoginPage";
 import NotFound from "@/pages/error/NotFound";
 
@@ -22,25 +34,39 @@ const routes: RouteObject[] = [
     element: <AuthGuard allowRole="student" />,
     children: [
       {
-        //   element: <StudentLayout />,
-        //   children: [
-        //     {
-        //       index: true,
-        //       element: <Navigate to="lab" replace />, // 自动跳转到 /student/lab
-        //     },
-        //     {
-        //       path: "lab",
-        //       element: <LabPage />,
-        //     },
-        //     {
-        //       path: "lab/:id",
-        //       element: <ExperimentWorkbench />,
-        //     },
-        //     {
-        //       path: "dashboard",
-        //       element: <StudentDashboard />,
-        //     },
-        //   ],
+        element: <StudentLayout />,
+        children: [
+          {
+            index: true,
+            element: <Navigate to="lab" replace />,
+          },
+          {
+            path: "lab",
+            element: <LabPage />,
+            children: [
+              {
+                path: ":labId/pre",
+                element: <PreStagePage />,
+              },
+              {
+                path: ":labId/doing",
+                element: <DoingStagePage />,
+              },
+              {
+                path: ":labId/post",
+                element: <PostStagePage />,
+              },
+            ],
+          },
+          {
+            path: "feedback",
+            element: <FeedbackPage />,
+          },
+          {
+            path: "profile",
+            element: <ProfilePage />,
+          },
+        ],
       },
     ],
   },
@@ -51,17 +77,21 @@ const routes: RouteObject[] = [
     element: <AuthGuard allowRole="teacher" />,
     children: [
       {
-        //   element: <TeacherLayout />,
-        //   children: [
-        //     {
-        //       index: true,
-        //       element: <Navigate to="dashboard" replace />, // 自动跳转到 /teacher/dashboard
-        //     },
-        //     {
-        //       path: "dashboard",
-        //       element: <Dashboard />,
-        //     },
-        //   ],
+        element: <TeacherLayout />,
+        children: [
+          {
+            index: true,
+            element: <Navigate to="dashboard" replace />, // 自动跳转到 /teacher/dashboard
+          },
+          {
+            path: "dashboard",
+            element: <Dashboard />,
+          },
+          {
+            path: "help",
+            element: <HelpPage />,
+          },
+        ],
       },
     ],
   },
