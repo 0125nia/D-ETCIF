4c9cb71c7c5e6bed95a917f41c1f0907ef692467
0125nia <1205367274@qq.com>
2026-03-27 05:45:38 +0800

    feat(frontend:index): add ReportUpload component export to base index

--- D-ETCIF-frontend/src/components/base/index.tsx
+++ D-ETCIF-frontend/src/components/base/index.tsx
@@ -1,3 +1,4 @@
 export { default as HelpModal } from "./HelpModal";
 export { default as StageTag } from "./StageTag";
 export { default as TopNav } from "./TopNav";
+export { default as ReportUpload } from "./ReportUpload";
