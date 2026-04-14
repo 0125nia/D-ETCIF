07afebcf24a5474d7c6c0f31baef12d06d399769
0125nia <1205367274@qq.com>
2026-04-14 10:57:01 +0800

    feat(backend:main): reorganize imports and enhance initialization flow for database and service

--- D-ETCIF-backend/cmd/main.go
+++ D-ETCIF-backend/cmd/main.go
@@ -1,20 +1,29 @@
-// Package cmd D-ETCIF-backend/cmd/main.go
+// Package main
+// D-ETCIF-backend/cmd/main.go
 package main
 
 import (
 	config "D-ETCIF-backend/configs"
+	"D-ETCIF-backend/internal/repository"
 	"D-ETCIF-backend/internal/router"
 	"D-ETCIF-backend/pkg/utils"
 
 	db_config "D-ETCIF-backend/internal/config"
+	"D-ETCIF-backend/internal/service"
 )
 
 func main() {
 	utils.Info("========开始初始化数据库连接========")
 
 	db_config.InitDB()
+	
+	repository.Migrate()
 
 	utils.Info("========数据库连接初始化完成========")
+
+	utils.Info("========初始化规则引擎========")
+	service.InitEngine()
+
 	r := router.NewRouter()
 
 	utils.Info("========启动服务========")
