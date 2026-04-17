bc4bc115f8952c0c06f7c13bba6ffd9be671b4bb
0125nia <1205367274@qq.com>
2026-04-17 21:30:47 +0800

    fix(frontend:types): update experiment_stage type to support both number and string

--- D-ETCIF-frontend/src/types/help.ts
+++ D-ETCIF-frontend/src/types/help.ts
@@ -3,7 +3,7 @@ export interface HelpDetail {
   user_id: number;
   username?: string; // 可选，如果后端做了关联查询建议带上
   experiment_id: number;
-  experiment_stage: string; // 对应 PRE / DOING / POST
+  experiment_stage: number | string; // 对应 PRE / DOING / POST
   title: string;
   content: string;
   status?: number; // 0: 待处理, 1: 已解决 (建议预留)
